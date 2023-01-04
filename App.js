3d8043b79a8fba4571654696e76872c4f444f905
lindsayPeeters <lindsay.99.peeters@gmail.com>
2023-01-04 17:44:52 +0100

    multiple screens

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+//screens
+import DrinksScreen from './screens/drinksScreen';
+import DetailScreen from './screens/detailScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Drinks</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Drinks" component={DrinksScreen}/>
+        <Stack.Screen name="Details" component={DetailScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ screens/detailScreen.js
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, FlatList, TextInput } from 'react-native';
+
+const DetailScreen = () =>{
+
+}
+export default DetailScreen;
